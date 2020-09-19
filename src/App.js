8db8a49a78f512ddc2f39c2dd236c808f8d0060c
Andreea Furtuna <furtuna.andreeac@gmail.com>
2020-09-19 15:39:21 +0200

    Created the Github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Zen App</div>;
+  return (
+    <div className="App">
+      React Zen App
+      <footer>
+        Coded by Andreea Furtuna and is open-sourced on {""}
+        <a
+          href="https://github.com/Andreea-Furtuna/react-zen-app"
+          target="_blank"
+        >
+          {""}GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
