950502d3953a3f1af47532dc97fb7adabe016f91
Andreea Furtuna <furtuna.andreeac@gmail.com>
2020-09-19 23:00:53 +0200

    Added style

--- src/Weather.js
+++ src/Weather.js
@@ -4,13 +4,12 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>Amsterdam</h1>
       <form>
         <div className="row">
           <div className="col-9">
             <input
               type="text"
-              placeholder="Type here your city..."
+              placeholder="Type here your city...✍︎"
               className="form-control"
             />
           </div>
@@ -19,24 +18,41 @@ export default function Weather() {
           </div>
         </div>
       </form>
-      <ul>
-        <li>Saturday 16:01</li>
-        <li>Cloudy</li>
-      </ul>
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="Cloudy"
-          />
-          6° C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
-          <ul>
-            <li>Description: Cloudy</li>
-            <li>Humidity: 10%</li>
-            <li>Wind: 3km/h</li>
-          </ul>
+          <h1>Amsterdam</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="card">
+            <ul>
+              <li>Saturday 16:01</li>
+              <li>Cloudy</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card">
+            <ul>
+              <li>Description: Cloudy</li>
+              <li>Humidity: 10%</li>
+              <li>Wind: 3km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
