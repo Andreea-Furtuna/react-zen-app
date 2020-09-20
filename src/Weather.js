34c6813a5a6556de7acc84eb34a02230983425e4
Andreea Furtuna <furtuna.andreeac@gmail.com>
2020-09-20 22:25:53 +0200

    Created the WeatherInfo component and making search engine functional

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Loader from "react-loader-spinner";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -19,16 +20,32 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "e712c02183c40bcef18f9c8f32b75f7a";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="text"
                 placeholder="Type here your city...✍︎"
                 className="form-control"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -36,53 +53,19 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <div className="row">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <span className="temperature">
-                  {Math.round(weatherData.temperature)}
-                </span>
-                <span className="unit">°C</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            <h1>{weatherData.city}</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <div className="card">
-              <ul>
-                <li>
-                  <FormattedDate date={weatherData.date} />
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="card">
-              <ul>
-                <li className="text-capitalize">{weatherData.description}</li>
-                <li>Humidity: {weatherData.humidity}%</li>
-                <li>Wind: {weatherData.wind}km/h</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "e712c02183c40bcef18f9c8f32b75f7a";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return <Loader type="ThreeDots" color="black" height={60} width={60} />;
+    search();
+    return (
+      <Loader
+        type="ThreeDots"
+        color="black"
+        height={60}
+        width={60}
+        text-align="center"
+      />
+    );
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,49 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src={props.data.iconUrl}
+              alt={props.data.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <h1>{props.data.city}</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="card">
+            <ul>
+              <li>
+                <FormattedDate date={props.data.date} />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card">
+            <ul>
+              <li className="text-capitalize">{props.data.description}</li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind}km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
